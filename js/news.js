4e2a344b26f92e11aa58d134fffbb290abcc8acf
Your Name <you@example.com>
2022-09-11 22:20:43 +0600

    modal add

--- js/news.js
+++ js/news.js
@@ -1,18 +1,18 @@
-const newsCategoryLoad = () => {
+const loadCategory = () => {
     const url = `https://openapi.programming-hero.com/api/news/categories`;
     fetch(url)
         .then(res => res.json())
-        .then(data => displayNewsCategory(data.data.news_category))
-        .catch(error => console.log(Data not load because ${ error }))
+        .then(data => displayCategory(data.data.news_category))
+        .catch(error => console.log(error))
 }
-const displayNewsCategory = categories => {
+const displayCategory = categories => {
     //console.log(categories);
     const categoryContainer = document.getElementById('category-container');
     categories.forEach(category => {
         const categoryDiv = document.createElement('div');
         categoryDiv.classList.add('container-fluid');
         categoryDiv.innerHTML = `
-        <a class="navbar-brand blog navbar" onclick="newsLoad('${category.category_id}')" href="#">${category.category_name}</a>
+        <a class="navbar-brand blog navbar" onclick="loadNews('${category.category_id}')" href="#">${category.category_name}</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
             aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -22,18 +22,18 @@ const displayNewsCategory = categories => {
     })
 };
 
-const newsLoad = (categoryId) => {
-    const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`;
+const loadNews = (category_id) => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
     fetch(url)
         .then(res => res.json())
         .then(data => displayNews(data.data))
-        .catch(error => console.log(Data not load because ${ error }));
+        .catch(error => console.log(error))
 }
 
 const displayNews = newses => {
     const newsContainer = document.getElementById('news-container');
     newses.forEach(news => {
-        console.log(news);
+        //console.log(news);
         const newsDiv = document.createElement('div');
         newsDiv.classList.add('card');
         newsDiv.innerHTML = `
@@ -70,7 +70,7 @@ const displayNews = newses => {
             </p>
         </div>
         <div>
-            <a href="#"><i class="fa-solid fa-arrow-right" onclick="loadNewsDetails()"></i></a>
+            <a href="#"><i class="fa-solid fa-arrow-right" onclick="loadNewsDetails('${news._id}')" data-bs-toggle="modal" data-bs-target="#newsDetailModal"></i></a>
         </div>
         
             
@@ -83,8 +83,51 @@ const displayNews = newses => {
     })
 };
 
-const loadNewsDetails = () => {
+const loadNewsDetails = (news_id) => {
+    const url = `https://openapi.programming-hero.com/api/news/${news_id}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayNewsDetails(data.data))
+}
+const displayNewsDetails = allNewsDetails => {
+    const modalBody = document.getElementById('modal-body');
 
+    allNewsDetails.forEach(newsDetails => {
+
+        modalBody.innerHTML = `
+        <div class="col-md-12">
+        <img src="${newsDetails.image_url}" class="img-fluid rounded-start" alt="...">
+      </div>
+      <h2 class="card-title my-3">${newsDetails.title ? newsDetails.title : 'No Data Found'}</h2>
+      <p class="card-text">${newsDetails.details ? newsDetails.details : 'No Data Found'}</p>
+      <div class="style2"><small class="p-3">Rating : ${newsDetails.rating.number}
+      </small>
+      <small class="p-3">Badge : ${newsDetails.rating.badge ? newsDetails.rating.badge : 'No Data Found'}</small>
+      </div>
+
+        <div class="card-text style2 my-2">
+
+            <div>
+                <div style="width:40px" class="d-inline-block">
+                    <img src="${newsDetails.author.img ? newsDetails.author.img : 'No Data Found'}"
+                        class="img-fluid rounded-circle ">
+                </div>
+                <span>${newsDetails.author.name ? newsDetails.author.name : 'No Data Found'}</span>
+            </div>
+
+            <div>
+                <span>
+                    <p><i class="fa-light fa-eye"></i></p>
+                </span>
+                <span>
+                    ${newsDetails.total_view ? newsDetails.total_view : 'No Data Found'}
+                </span>
+            </div>
+            </div>
+ `
+    })
 }
 
-newsCategoryLoad();
+loadCategory();
+loadNews();
+loadNewsDetails();
